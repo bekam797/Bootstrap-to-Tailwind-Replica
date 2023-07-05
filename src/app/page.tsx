5249af5ce5b1b1ff90c3255b4371c4d4931694c9
Beka <beka@Bekas-MacBook-Pro.local>
2023-07-06 01:05:01 +0400

    change font family and add relevant text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@
 import { useState } from "react";
 import { Collapse } from "react-collapse";
 
-const ITEMS = [1, 2, 3];
+const ITEMS = ["first", "second", "third"];
 
 export default function Home() {
   const [openIndex, setOpenIndex] = useState<any>(null);
 
   return (
-    <main className="flex min-h-screen flex-col p-8 bg-[#f8f9fa] sm:p-24">
+    <main className="flex min-h-screen flex-col p-4 bg-[#f8f9fa] sm:p-24">
       <h2 className="py-6">TailwindCss Accordion</h2>
       <div className="bg-white rounded">
         {ITEMS.map((item, index) => (
@@ -26,21 +26,23 @@ export default function Home() {
               className={`${
                 openIndex === index &&
                 "text-[#0c63e4] bg-[#e7f1ff] shadow-[inset_0_-1px_0_rgba(0,0,0,.125)]"
-              } py-3 px-4 text-base font-medium w-full text-left flex justify-between items-center`}
+              } py-3 px-4 text-base font-normal w-full text-left flex justify-between items-center`}
             >
-              Accordion Item #{item}
+              Accordion Item #{index + 1}
               <span
                 className={openIndex === index ? "arrow-up" : "arrow-down"}
               ></span>
             </button>
             <Collapse isOpened={openIndex === index}>
               <div className="py-4 px-6">
-                <strong>This is the {item} item's accordion body.</strong> You
-                can modify any of this with custom CSS or overriding our default
-                variables. It's also worth noting that just about any HTML can
-                go within the{" "}
-                <code className="text-[#d63384] text-sm">.accordion-body</code>,
-                though the transition does limit overflow.
+                <strong>This is the {item} item's accordion body.</strong> It is shown by default,
+                until the collapse plugin adds the appropriate classes that we
+                use to style each element. These classes control the overall
+                appearance, as well as the showing and hiding via CSS
+                transitions. You can modify any of this with custom CSS or
+                overriding our default variables. It's also worth noting that
+                just about any HTML can go within the <code className="text-[#d63384] text-sm">.accordion-body</code>, though
+                the transition does limit overflow.
               </div>
             </Collapse>
           </div>
